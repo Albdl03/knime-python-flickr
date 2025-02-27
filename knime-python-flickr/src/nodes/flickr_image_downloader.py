68ae59b9f0b59319d672c59a85e1916d0bfc82fc
Alberto de Leo <deleoa@tcd.ie>
2025-02-27 11:01:03 +0000

    Update flickr_image_downloader.py

--- knime-python-flickr/src/nodes/flickr_image_downloader.py
+++ knime-python-flickr/src/nodes/flickr_image_downloader.py
@@ -8,7 +8,7 @@ LOGGER = logging.getLogger(__name__)
 
 # Define sub-category
 image_category = knext.category(
-    path="/community/vit_ft",
+    path="/community/flickr_images",
     level_id="flickrimg",
     name="Flickr Image Downloader",
     description="Node for downloading image URLs from Flickr",
